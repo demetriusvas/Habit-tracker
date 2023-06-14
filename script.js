e14ad3b59748d6abd2dc0994792c3a552d58dd9a
Demetrius Vignati <demetriusvas@gmail.com>
2023-06-13 22:13:01 -0300

    Clique dos botões OK

--- script.js
+++ script.js
@@ -1,6 +1,107 @@
-const botao = document.getElementById("circle1");
-botao.addEventListener("click", trocarCor);
+function mudarCor(circle){
+//     if(circle == 'circle1'){
+//         document.getElementById('circle1').className = 'button-clicked';
+//     } else if(circle == 'circle2'){
+//         document.getElementById('circle2').className = 'button-clicked';
+//     }else if(circle == 'circle3'){
+//         document.getElementById('circle3').className = 'button-clicked';
+//     }
+    switch(circle){
+        case 'circle1':
+            document.getElementById('circle1').className = 'button-clicked';
+            break;
+        case 'circle2':
+            document.getElementById('circle2').className = 'button-clicked';
+            break;
+        case 'circle3':
+            document.getElementById('circle3').className = 'button-clicked';
+            break;
+        case 'circle4':
+            document.getElementById('circle4').className = 'button-clicked';
+            break;
+        case 'circle5':
+            document.getElementById('circle5').className = 'button-clicked';
+            break;
+        case 'circle6':
+            document.getElementById('circle6').className = 'button-clicked';
+            break;
+        case 'circle7':
+            document.getElementById('circle7').className = 'button-clicked';
+            break;
+        case 'circle8':
+            document.getElementById('circle8').className = 'button-clicked';
+            break;
+        case 'circle9':
+            document.getElementById('circle9').className = 'button-clicked';
+            break;
+        case 'circle10':
+            document.getElementById('circle10').className = 'button-clicked';
+            break;
+        case 'circle11':
+            document.getElementById('circle11').className = 'button-clicked';
+            break;
+        case 'circle12':
+            document.getElementById('circle12').className = 'button-clicked';
+            break;
+        case 'circle13':
+            document.getElementById('circle13').className = 'button-clicked';
+            break;
+        case 'circle14':
+            document.getElementById('circle14').className = 'button-clicked';
+            break;
+        case 'circle15':
+            document.getElementById('circle15').className = 'button-clicked';
+            break;
+        case 'circle16':
+            document.getElementById('circle16').className = 'button-clicked';
+            break;
+        case 'circle17':
+            document.getElementById('circle17').className = 'button-clicked';
+            break;
+        case 'circle18':
+            document.getElementById('circle18').className = 'button-clicked';
+            break;
+        case 'circle19':
+            document.getElementById('circle19').className = 'button-clicked';
+            break;
+        case 'circle20':
+            document.getElementById('circle20').className = 'button-clicked';
+            break;
+        case 'circle21':
+            document.getElementById('circle21').className = 'button-clicked';
+            break;
+        case 'circle22':
+            document.getElementById('circle22').className = 'button-clicked';
+            break;
+        case 'circle23':
+            document.getElementById('circle23').className = 'button-clicked';
+            break;
+        case 'circle24':
+            document.getElementById('circle24').className = 'button-clicked';
+            break;
+        case 'circle25':
+            document.getElementById('circle25').className = 'button-clicked';
+            break;
+        case 'circle26':
+            document.getElementById('circle26').className = 'button-clicked';
+            break;
+        case 'circle27':
+            document.getElementById('circle27').className = 'button-clicked';
+            break;
+        case 'circle28':
+            document.getElementById('circle28').className = 'button-clicked';
+            break;
+        case 'circle29':
+            document.getElementById('circle29').className = 'button-clicked';
+            break;
+        case 'circle30':
+            document.getElementById('circle30').className = 'button-clicked';
+            break;
+        case 'circle31':
+            document.getElementById('circle31').className = 'button-clicked';
+            break;
+        default:
+            console.log("Fim");
+    }
 
-function trocarCor(){
-    document.getElementById("circle1").className = 'circle1_checked';
 }
